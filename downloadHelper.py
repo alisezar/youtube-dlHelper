84bd0defe0c7f6d33209f8647fc934a86a884fd9
Ali Sezar <kingqwe8@gmail.com>
2018-10-18 11:17:00 +0330

    fixed and added options

--- downloadHelper.py
+++ downloadHelper.py
@@ -15,28 +15,30 @@ def dwnOut(link,Type):
 #get input
 li=input("send link to video: ")
 Vtype=input("A audio\nB bad audio\nC best quality video\n"
-            "D bad quality video\nE video thumbnail\n"
-            "F type your command\nG video thumbnail\n"
-            "H list formats\nI video 720 if available"
+            "D bad quality video\nE video thumbnail and video\n"
+            "F type your command\nG list formats\n"
+            "H video 720 if available\nI video 480 if available\n"
+            "j video 360 if available\nK thumbnail only\n"
+            "0 For No Command\n"
             "\nplease choose what you want: "
             )
 Vtype=Vtype.lower()
 #check input and run
 if (Vtype=="a"):
-    dwnOut(li," -f bestaudio ")
-    #dwn(li," -f bestaudio ")
+    dwnOut(li," -f bestaudio -c")
+    #dwn(li," -f bestaudio -c")
 elif (Vtype=="b"):
     dwnOut(li," -f worstaudio ")
-    #dwn(li," -f worstaudio ")
+    #dwn(li," -f worstaudio -c")
 elif (Vtype=="c"):
-    dwnOut(li," -f best ")
-    #dwn(li," -f best ")
+    dwnOut(li," -f best -c")
+    #dwn(li," -f best -c")
 elif (Vtype=="d"):
-    dwnOut(li," -f worst ")
-    #dwn(li," -f worst ")
+    dwnOut(li," -f worst -c")
+    #dwn(li," -f worst -c")
 elif (Vtype=="e"):
-    dwnOut(li," --write-thumbnail ")
-    #dwn(li," --write-thumbnail ")
+    dwnOut(li," --write-thumbnail -c")
+    #dwn(li," --write-thumbnail -c")
 elif (Vtype=="f"):
     cmd=input("Enter your commad")
     if(cmd[0]!=" "):
@@ -44,18 +46,22 @@ elif (Vtype=="f"):
     dwnOut(li,cmd)
     #dwn(li,cmd)
 elif (Vtype=="g"):
-    dwnOut(li," --list-formats ")
+    dwnOut(li," --list-formats -c")
+    input("perss any key to continue")
     #dwn(li," --list-formats ")
-elif (Vtype=="g"):
-    dwnOut(li," -f best[height=720] ")
+elif (Vtype=="h"):
+    dwnOut(li," -f best[height=720] -c")
     #dwn(li," -f best[height=720] ")
-elif (Vtype=="j"):
-    dwnOut(li," -f best[height=480] ")
+elif (Vtype=="i"):
+    dwnOut(li," -f best[height=480] -c")
     #dwn(li," -f best[height=480] ")
-elif (Vtype=="k"):
-    dwnOut(li," -f best[height=360] ")
+elif (Vtype=="j"):
+    dwnOut(li," -f best[height=360] -c")
     #dwn(li," -f best[height=360] ")
 elif (Vtype=="0"):
     dwnOut(li,"")
+elif (Vtype=="k"):
+    dwnOut(li," --write-thumbnai --skip-download ")
 else:
     print("command not find")
+input("send any key to close!")
